20ed710c5c3424a24938b998e4df52c3060cc072
Inna <shvartz_i@mail.ru>
2022-01-27 13:49:51 +0300

    комментарии в задачах 1 и 2

--- Task1_Natchisla/Program.cs
+++ Task1_Natchisla/Program.cs
@@ -1,10 +1,6 @@
 ﻿// Вывести цифры от 1 до N
 int NaturalChisla(int n)
 {
-    //if (n == 1)
-    // return n;
-    //else
-    // return NaturalChisla(n-1) + 1;
     return n==1 ? n : NaturalChisla(n-1) + 1;
 }
 Console.Write("Введите N: ");
--- Task2_SummOfDigits/Program.cs
+++ Task2_SummOfDigits/Program.cs
@@ -2,13 +2,13 @@
 // Сумма цифр числа N
 
 int Function (int n) => n<10 ? n : Function(n/10) + n%10;
-{
-    // можно так: 
+// это значит:
+//{  
     // if (n<10) return n;
     // else return Function(n/10) + n%10;
     // или так:
     // return n<10 ? n : Function(n/10) + n%10;
-}
+//}
 Console.Write ("Введите число N: ");
 int N = int.Parse(Console.ReadLine() ?? "0");
 Console.WriteLine($"Сумма цифр числа {N} равна {Function(N)}");
