df534d4f742dbcb2e3ef699f1ce3bb5cef48c542
Inna <shvartz_i@mail.ru>
2022-02-02 06:25:27 +0300

    задачи 1 и 9(Словосочетания)

---
+++ Task1_NatchislaVpered/Program.cs
@@ -0,0 +1,15 @@
+﻿// Вывести нат.числа от 1 до N
+
+void NatChisla (int i,int max)
+{
+    if (i<=max)
+    {
+        Console.WriteLine (i);
+        NatChisla (i+1,max);
+    }
+}
+Console.Write("Введите N: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+int i = 1;
+NatChisla (i,N);
+
